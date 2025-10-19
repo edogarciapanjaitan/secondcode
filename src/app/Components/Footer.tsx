0f30752eec28f3ea47b5065f081c6df0625ac256
edogarciapanjaitan <edson.bennetgarcia@gmail.com>
2025-10-20 04:40:52 +0700

    secondcode

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
+        <span className="text-2xl">Edo Garcia</span>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
