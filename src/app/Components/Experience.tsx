012ec3c648d5ee58a8dff01752a1e83f1bfc9200
edogarciapanjaitan <edson.bennetgarcia@gmail.com>
2026-02-07 19:49:31 +0700

    perbaikan navbar

--- src/app/Components/Experience.tsx
+++ src/app/Components/Experience.tsx
@@ -8,21 +8,21 @@ const Experience = () => {
         <div className="flex flex-col md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:py-16 mt-20 " id="experience">
             <img className="w-[370px] h-[370px]" src=".\assets\3779552.jpg" alt="pic"></img>
             <h1 className="text-transparent text-4xl font-bold mb-4 bg-clip-text bg-gradient-to-l from-slate-600 to-slate-300">
-            Professional Experience
-            <ul className="text-[20px] mt-2">Full-stack Web Developer PT. Nutrifood Indonesia</ul>
-            <ul className="text-[15px] mt-1">Responsibilities & Key Contributions:</ul>
-                <li className="text-[13px] mt-1 px-2">Developed and maintained both front-end and back-end components of high-traffic web applications using React.js/Vue.js and Node.js with Express with Django.</li>
-                <li className="text-[13px] mt-1 px-2">Designed and managed SQL/NoSQL databases, optimizing queries and ensuring data integrity and security.</li>
-                <li className="text-[13px] mt-1 px-2">Collaborated with design and product teams to translate UI/UX wireframes into responsive and user-friendly interfaces.</li> 
+                Professional Experience
+                <ul className="text-[20px] mt-2">Sales Operation PT. Nutrifood Indonesia (2010 - 2025)</ul>
+                <ul className="text-[15px] mt-1">Responsibilities & Key Contributions:</ul>
+                <li className="text-[13px] mt-1 px-2">Streamlined internal sales processes to improve operational efficiency and team productivity.</li>
+                <li className="text-[13px] mt-1 px-2">Collaborated with cross-functional teams to identify bottlenecks in the sales funnel and implemented systemic solutions.</li>
+                <li className="text-[13px] mt-1 px-2">Managed complex datasets and generated daily reports to support data-driven decision-making for the management team.</li>
 
 
-            <ul className="text-[20px] mt-10">Junior Web Developer</ul>
-            <ul className="text-[15px] mt-1">Responsibilities & Key Contributions:</ul>
-                <li className="text-[13px] mt-1 px-2">Assisted senior developers in building new features and functionalities using HTML5, CSS3, and JavaScript (ES6+).</li>
-                <li className="text-[13px] mt-1 px-2">Fixed bugs and performed application maintenance, improving overall site performance and reliability.</li>
-                <li className="text-[13px] mt-1 px-2">Contributed to the development of responsive designs, ensuring cross-browser compatibility across various devices.</li>  
-            </h1>   
-    </div>       
+                <ul className="text-[20px] mt-10">Web Developer</ul>
+                <ul className="text-[15px] mt-1">Responsibilities & Key Contributions:</ul>
+                <li className="text-[13px] mt-1 px-2">Built responsive and interactive user interfaces with React.js and Tailwind CSS, improving user engagement metrics..</li>
+                <li className="text-[13px] mt-1 px-2">Designed and implemented robust RESTful APIs and database schemas using Node.js and PostgreSQL/MongoDB.</li>
+                <li className="text-[13px] mt-1 px-2">Optimized application performance and ensured cross-browser compatibility and mobile responsiveness.</li>
+            </h1>
+        </div>
     );
 }
 
--- src/app/Components/NavBar.tsx
+++ src/app/Components/NavBar.tsx
@@ -5,28 +5,28 @@ import React from "react"
 
 const NavBar = () => {
     return (
-        <div className="navbar flex items-center justify-between fixed top-0 text-white w-full">
-           
-                <h1 className="ml-9 text-2xl font-semibold">
-                    <button>
-                        <a href="#">Edo Garcia</a>
-                    </button>
-                </h1>
-            
-                <ul className="flex items-center gap-10 md:static fixed mr-9">
-                    <li>
-                        <a href="#aboutme">About Me</a>
-                    </li>
-                    <li>
-                        <a href="#skills">Skills</a>
-                    </li>
-                    <li>    
-                        <a href="#experience">Experience</a>
-                    </li>
-                </ul>
-            </div>
+        <div className="navbar flex items-center justify-between fixed top-0 left-0 right-0 z-50 bg-[#121212] bg-opacity-100 text-white w-full">
 
-        
+            <h1 className="ml-9 text-2xl font-semibold">
+                <button>
+                    <a href="#">Edo Garcia</a>
+                </button>
+            </h1>
+
+            <ul className="flex items-center gap-10 md:static fixed mr-9">
+                <li>
+                    <a href="#aboutme">About Me</a>
+                </li>
+                <li>
+                    <a href="#skills">Skills</a>
+                </li>
+                <li>
+                    <a href="#experience">Experience</a>
+                </li>
+            </ul>
+        </div>
+
+
     );
 
 }
